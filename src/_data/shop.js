20fdeed5a267e34db3e1b49800274d64f40899f6
eoFranca <erfanca.com@gmail.com>
2024-10-08 15:11:58 -0300

    preparação para interligação com o backend

--- src/_data/shop.js
+++ src/_data/shop.js
@@ -2,60 +2,68 @@ export const clothing = [
     {
         id:1,
         nome:"Beanie Popped",
+        descricao:"",
         valor: 30,
-        cor: "Black",
-        img:  "https://i.ibb.co/jM5qtk6/Beanie-Mockup-Front-and-Back-View-1.png"
+        categoria: "",
+        capa:  "https://i.ibb.co/jM5qtk6/Beanie-Mockup-Front-and-Back-View-1.png"
     },
     {
         id:2,
         nome:"Beanie Popped",
+        descricao:"",
         valor: 30,
-        cor: "White",
-        img:"https://i.ibb.co/nDvsB8Y/Beanie-Mockup-Front-and-Back-View-1-d.png"
+        categoria: "",
+        capa:"https://i.ibb.co/nDvsB8Y/Beanie-Mockup-Front-and-Back-View-1-d.png"
     },  
     {
         id:3,
         nome:"Cap Popped",
+        descricao:"",
         valor: 20,
-        cor: "White",
-        img: "https://i.ibb.co/2ygYvT2/Cap-Mockup-Front-And-Back-View-UV.png"
+        categoria: "",
+        capa: "https://i.ibb.co/2ygYvT2/Cap-Mockup-Front-And-Back-View-UV.png"
     },
     {
         id:4,
         nome:"Hoodie Popped",
+        descricao:"",
         valor: 130,
-        cor: "White",
-        img:"https://i.ibb.co/9GFfVsB/Men-Hoodie-Mockup-Front-View.png"
+        categoria: "",
+        capa:"https://i.ibb.co/9GFfVsB/Men-Hoodie-Mockup-Front-View.png"
     },
     {
         id:5,
         nome:"Hoodie Popped",
+        descricao:"",
         valor: 130,
-        cor: "Black",
-        img: "https://i.ibb.co/543kVD1/Men-Hoodie-Mockup-Front-View-1.png" 
+        categoria: "",
+        capa: "https://i.ibb.co/543kVD1/Men-Hoodie-Mockup-Front-View-1.png" 
     },{
 
         id:6,
         nome:"T-Shirt PoppedBoy",
+        descricao:"",
         valor: 70,
-        cor: "Black",
-        img: "https://i.ibb.co/4tYK6kQ/Men-T-Shirt-Mockup-Front-View-2.png" 
+        categoria: "",
+        capa: "https://i.ibb.co/4tYK6kQ/Men-T-Shirt-Mockup-Front-View-2.png" 
     },{
 
         id:7,
         nome:"T-Shirt PoppedBoy",
+        descricao:"",
         valor: 70,
-        cor: "White",
-        img: "https://i.ibb.co/HdXbfWz/Men-T-Shirt-Mockup-Front-View-3.png" 
+        categoria: "",
+        capa: "https://i.ibb.co/HdXbfWz/Men-T-Shirt-Mockup-Front-View-3.png" 
     },{
 
         id:8,
         nome:"Nike Air Force 1 (Especial Edition Popped)",
+        descricao:"",
         valor: 200,
-        cor: "White",
-        img: "https://i.ibb.co/KhGtvY2/Nike-Air-Force-1-Mockup-Poster.png"
+        categoria: "",
+        capa: "https://i.ibb.co/KhGtvY2/Nike-Air-Force-1-Mockup-Poster.png"
     }
-    
+        
 
 
 ]
@@ -64,24 +72,27 @@ export const cups = [
     {
         id:1,
         nome:"Mug Popped",
+        descricao:"",
         valor: 10,
-        cor: "White",
-        img: "https://i.ibb.co/PWRsH7L/Mug-Mockup-Front-View.png"
+        categoria: "",
+        capa: "https://i.ibb.co/PWRsH7L/Mug-Mockup-Front-View.png"
     },
     {
         id:2,
         nome:"Mug Popped",
+        descricao:"",
         valor: 10,
-        cor: "Black",
-        img: "https://i.ibb.co/zPGcj4N/Mug-Mockup-Front-View-1.png" 
+        categoria: "",
+        capa: "https://i.ibb.co/zPGcj4N/Mug-Mockup-Front-View-1.png" 
 
     },
     {
         id:3,
         nome:"Paper Cup Popped",
+        descricao:"",
         valor: 2,
-        cor: "White",
-        img: "https://i.ibb.co/RjxWdjY/Paper-Cups-Mockup.png" 
+        categoria: "",
+        capa: "https://i.ibb.co/RjxWdjY/Paper-Cups-Mockup.png" 
         
     }
 ]
@@ -89,23 +100,26 @@ export const bags = [
     {
         id:1,
         nome:"Tote Bag PoppedBoy",
+        descricao:"",
         valor: 12,
-        cor: "White",
-        img: "https://i.ibb.co/8r5VcWY/Tote-Bag-Mockup-Front-View.png" 
+        categoria: "",
+        capa: "https://i.ibb.co/8r5VcWY/Tote-Bag-Mockup-Front-View.png" 
     },
      {
         id:2,
         nome:"Tote Bags Popped ",
+        descricao:"",
         valor: 12,
-        cor: "White and Black",
-        img: "https://i.ibb.co/z6mDzhk/Tote-Bags-Mockup-Falling-1.png"
+        categoria: "",
+        capa: "https://i.ibb.co/z6mDzhk/Tote-Bags-Mockup-Falling-1.png"
     }
     ,
      {
         id:3,
         nome:"Shopping Bags Popped ",
+        descricao:"",
         valor: 7,
-        cor: "White",
-        img: "https://i.ibb.co/ZWJGZDL/Shopping-Bags-Mockup.png" 
+        categoria: "",
+        capa: "https://i.ibb.co/ZWJGZDL/Shopping-Bags-Mockup.png" 
     }
 ]
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import LayoutFull from '@/layouts/LayoutFull.vue';
 import LoginView from '@/views/LoginView.vue'
 import LogoutView from '@/views/LogoutView.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -36,7 +37,9 @@ const router = createRouter({
       path: '/logout',
       name: 'logout',
       component: LogoutView
-    }
+    },
+
+
   ]
 })
 
