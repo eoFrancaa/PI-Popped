a9b7867b72397732263738d23549240688552d9e
dudugazuforget <dudu99744@gmail.com>
2024-10-18 09:05:24 -0300

    implementação das imagens do banco de dados e adicionando o carrossel

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue';
 import { useProductStore } from '@/stores/product'
 import { watch } from 'vue';
+import 'vue-snap/dist/vue-snap.css';
 
 const props = defineProps(['category'])
 
@@ -32,11 +33,12 @@ const addToCart = (product) => {
     <carousel>
       <div v-for="product in productStore.products" :key="product.id" class="clouth">
         <slide>
-          <div class="card">
-            <p>{{ product.nome }}</p>
-            <p>{{ product.valor }}$</p>
-            <button class="b-buy" @click="addToCart(product)">Add to Cart</button>
-          </div>
+            <div class="card">
+              <img :src="product.capa.url">
+              <p>{{ product.nome }}</p>
+              <p>{{ product.valor }}$</p>
+              <button class="b-buy" @click="addToCart(product)">Add to Cart</button>
+            </div>
         </slide>
       </div>
     </carousel>
