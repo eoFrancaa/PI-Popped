c8d0b55ddaea87c333086573f6c5d8f0af3571a2
dudug05 <dudugx05@gmail.com>
2024-12-12 13:04:13 -0300

    pagina discovery

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u39ndec0n2"
+    "revision": "0.jtoe4317hp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
